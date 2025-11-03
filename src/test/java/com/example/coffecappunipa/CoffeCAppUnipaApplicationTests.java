f9321892a34fa2836e0150e14ef38afc683a92ea
Angelo <1234retiu@gmail.com>
2025-11-03 16:38:17 +0100

    firs assignment done
    created:
    -login page
    -registration page
    -client home
    -owner home
    -machine home
    -technician home

---
+++ src/test/java/com/example/coffecappunipa/CoffeCAppUnipaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffecappunipa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeCAppUnipaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
